5b0e4cdb3a6194c69c2a4afa1c8b568a85b9c30f
insist_anan <1530885478@qq.com>
2023-03-14 15:16:29 +0800

    tankver1

---
+++ Ver1/src/StartGame.java
@@ -0,0 +1,12 @@
+/**
+ * 开始游戏
+ *
+ * @author insis
+ * @date 2023/03/14
+ */
+public class StartGame {
+    public static void main(String[] args) {
+        GamePanel gamePanel = new GamePanel();
+        gamePanel.launch();
+    }
+}
